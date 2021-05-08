33d7af6c2cf6c790e9bb57dc12960e5de9d92e30
Diego Amicabile <diego.amicabile@amicabile.com>
2021-05-08 09:35:09 +0200

    Batch add

--- batch_add.py
+++ batch_add.py
@@ -8,7 +8,7 @@ from find_best_dinasty import find_best_dinasty
 if __name__ == "__main__":
     #s = sys.argv[1]
     #ck_people.add_person(s)
-    #ck_people.add_person("Faelan mac Briain,1084,1136,High King of Ireland,Briain")
+    ck_people.add_person("Faelan mac Brian,1084,1136,High King of Ireland,Briain")
     #ck_people.add_person("Rhufon ap Gruffydd,1140,1166,Petty King of Deheubarth,Caerloyw-Penfro")
     #ck_people.add_person("Elisabet Gudrodsdatter,1083,1148,,Crovan")
     #find_best_dinasty("Elisabet Crovan")
@@ -20,4 +20,6 @@ if __name__ == "__main__":
     #find_best_dinasty(person_mds, "Duke Haraldr I")
     #ck_people.add_person("Cynddylan ap Gruffydd,1099,1154,Lord of Merionnydd,Caerloyw-Penfro")
     #find_best_dinasty(person_mds, "Lord Cynddylan" )
-    find_best_dinasty(person_mds, "Petty King Gruffydd III" )
+    #find_best_dinasty(person_mds, "Petty King Gruffydd III" )
+    #ck_people.add_person("Sean mac Briain,1101,1135,Earl of Oriel,Briain")
+    #ck_people.add_person("Cathan nic Briain,1078,1144,Countess of Ennis,Briain")
