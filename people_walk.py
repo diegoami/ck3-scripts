6f52b39d9e7689150bdf370c83c4a1379d6268f3
Diego Amicabile <diego.amicabile@amicabile.com>
2021-05-30 06:42:52 +0200

    Walk, scripts

--- people_walk.py
+++ people_walk.py
@@ -80,68 +80,71 @@ def get_all_names(dir_mds=None, ppl_file=None, ck_people=None):
     mothers = defaultdict(set)
 
     for ck_person in ck_people:
-        person_stack = []
-        has_father = []
-        has_mother = []
-        level, spaces = 0, 0
-        full_file = os.path.join(dir_mds, ck_person.file_name)
-        olines, plines, alines = find_family_tree(full_file)
-        long_name_title = plines[0][2:].strip()
-        first_line = olines[0].strip()
-        if not (first_line == long_name_title == ck_person.long_name):
-            plines[0] = "# {}".format(ck_person.long_name)
-            olines[0] = ck_person.long_name
-            print("Fixing {}".format(ck_person.long_name))
-            with fileinput.FileInput(full_file, inplace=True) as file:
-                for line in file:
-                    print(line.replace(first_line, ck_person.long_name), end='')
-            death_fixes.add(ck_person)
-        person_stack.append(0)
+        try:
+            person_stack = []
+            has_father = []
+            has_mother = []
+            level, spaces = 0, 0
+            full_file = os.path.join(dir_mds, ck_person.file_name)
+            olines, plines, alines = find_family_tree(full_file)
+            long_name_title = plines[0][2:].strip()
+            first_line = olines[0].strip()
+            if not (first_line == long_name_title == ck_person.long_name):
+                plines[0] = "# {}".format(ck_person.long_name)
+                olines[0] = ck_person.long_name
+                print("Fixing {}".format(ck_person.long_name))
+                with fileinput.FileInput(full_file, inplace=True) as file:
+                    for line in file:
+                        print(line.replace(first_line, ck_person.long_name), end='')
+                death_fixes.add(ck_person)
+            person_stack.append(0)
 
-        long_name_to_file[ck_person.long_name] = ck_person.file_name
-        file_to_long_name[ck_person.file_name] = ck_person.long_name
-        long_names_in_years[int(ck_person.birth_year)].add(ck_person.long_name)
-        long_lines_in_years[int(ck_person.birth_year)].add(first_line)
+            long_name_to_file[ck_person.long_name] = ck_person.file_name
+            file_to_long_name[ck_person.file_name] = ck_person.long_name
+            long_names_in_years[int(ck_person.birth_year)].add(ck_person.long_name)
+            long_lines_in_years[int(ck_person.birth_year)].add(first_line)
 
-        for eindex, oline in enumerate(olines[1:]):
-            index = eindex+1
-            cspaces = len(oline)-len(oline.strip())
-            oline = oline.strip()
-            if ',' in oline:
-                name, years = [x.strip() for x in oline.strip().split(',')[:2]]
-                years_first = int(years.split('-')[0])
-                short_names_in_years[years_first].add(name)
-                short_lines_in_years[years_first].add(oline)
-                short_names_in_file[ck_person.file_name].add(name)
-                files_containing_short_names[name].add(ck_person.file_name)
-                if '  ' in oline:
-                    print('{}:{} : suspicious spaces'.format(ck_person.file_name, name))
-            if cspaces > spaces:
-                if person_stack[-1] in has_father:
-                    mothers[olines[person_stack[-1]].strip()].add(oline)
-                    has_mother.append(person_stack[-1])
-                else:
-                    fathers[olines[person_stack[-1]].strip()].add(oline)
-                    has_father.append(person_stack[-1])
-                spaces = cspaces
-                person_stack.append(index)
-            elif cspaces == spaces:
-                if person_stack[-2] in has_father:
-                    if not person_stack[-2] in has_mother:
-                        mothers[olines[person_stack[-2]].strip()].add(oline)
-                        person_stack.pop()
-            else:
-                person_stack.pop()
-                if person_stack[-1] in has_father:
-                    while person_stack and  person_stack[-1] in has_mother:
-                        person_stack.pop()
-                    if person_stack:
+            for eindex, oline in enumerate(olines[1:]):
+                index = eindex+1
+                cspaces = len(oline)-len(oline.strip())
+                oline = oline.strip()
+                if ',' in oline:
+                    name, years = [x.strip() for x in oline.strip().split(',')[:2]]
+                    years_first = int(years.split('-')[0])
+                    short_names_in_years[years_first].add(name)
+                    short_lines_in_years[years_first].add(oline)
+                    short_names_in_file[ck_person.file_name].add(name)
+                    files_containing_short_names[name].add(ck_person.file_name)
+                    if '  ' in oline:
+                        print('{}:{} : suspicious spaces'.format(ck_person.file_name, name))
+                if cspaces > spaces:
+                    if person_stack[-1] in has_father:
                         mothers[olines[person_stack[-1]].strip()].add(oline)
                         has_mother.append(person_stack[-1])
+                    else:
+                        fathers[olines[person_stack[-1]].strip()].add(oline)
+                        has_father.append(person_stack[-1])
+                    spaces = cspaces
+                    person_stack.append(index)
+                elif cspaces == spaces:
+                    if person_stack[-2] in has_father:
+                        if not person_stack[-2] in has_mother:
+                            mothers[olines[person_stack[-2]].strip()].add(oline)
+                            person_stack.pop()
+                else:
+                    person_stack.pop()
+                    if person_stack[-1] in has_father:
+                        while person_stack and  person_stack[-1] in has_mother:
+                            person_stack.pop()
+                        if person_stack:
+                            mothers[olines[person_stack[-1]].strip()].add(oline)
+                            has_mother.append(person_stack[-1])
 
-                person_stack.append(index)
-                spaces = cspaces
-        pass
+                    person_stack.append(index)
+                    spaces = cspaces
+            pass
+        except:
+            print("Error processing file {} at row .".format(ck_person.file_name, index))
     years_keys = sorted(long_names_in_years.keys())
     for key in years_keys:
         for long_name in long_names_in_years[key]:
